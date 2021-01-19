8de413dcf6c232b4387ede35c8128ee724b93ea8
Maria Searle <maria.searle@deeperthanblue.co.uk>
2021-01-19 15:07:39 +0000

    setting eggs in selector

--- src/pages/EggSelector.js
+++ src/pages/EggSelector.js
@@ -1,20 +1,32 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import data from '../schema.json'
 import {useHistory, useParams} from 'react-router'
 
 
 export default function EggSelector() {
     const history = useHistory();
+    const [eggs, setEggs] = useState({});
 
     function goBackHandle(){
         history.goBack();
     }
+
+    const getEggs = () => {
+        setEggs(data.eggs[0].icon)
+    }
+
+    useEffect(() => {
+        getEggs();
+    }, [])
 
     return (
         <div className="page-container">
             <div className="page-header">
                 <button onClick={goBackHandle} className="back-button"> Back </button>
             </div>
+
+            
+
             
         </div>
     )
