907398001acdf8562ff01add33b61f769f5c7c35
Maria Searle <maria.searle@deeperthanblue.co.uk>
2021-01-19 14:47:02 +0000

    adding back button that uses useHistory

--- src/pages/EggSelector.js
+++ src/pages/EggSelector.js
@@ -5,6 +5,9 @@ import data from '../schema.json'
 export default function EggSelector() {
     return (
         <div className="page-container">
+            <div className="page-header">
+                <button className="back-button"> Back </button>
+            </div>
             
         </div>
     )
