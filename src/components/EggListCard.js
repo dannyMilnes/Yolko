b98ada180506b6c76b1300d453c6ff781e639299
Maria Searle <maria.searle@deeperthanblue.co.uk>
2021-01-26 14:49:50 +0000

    setting egg selector into grid style

--- src/components/EggListCard.js
+++ src/components/EggListCard.js
@@ -5,12 +5,14 @@ import { Link } from 'react-router-dom'
 export default function EggListCard({egg}) { 
     //template for props and .map() of json to go through
     return (
-        <Link to={`/cooking-method/${egg.name}`}>
-            <div className="egg-card">
-                <img className="eggIcon" src={`assets/svg/${egg.icon}`} alt={egg.name}></img>
-                <p className="eggName">{egg.name}</p>
-            </div>
-        </Link>
+        <div className="eggItem">
+            <Link to={`/cooking-method/${egg.name}`}>
+                <div className="egg-card">
+                    <img className="eggIcon" src={`assets/svg/${egg.icon}`} alt={egg.name}></img>
+                    <p className="eggName">{egg.name}</p>
+                </div>
+            </Link>
+        </div>
     )
 }
 
